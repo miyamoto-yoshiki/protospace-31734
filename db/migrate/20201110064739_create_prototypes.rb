4188152dcf741f9f624974cfeaf574c12374a7c7
miyamoto-yoshiki <yo5shi2ki8@yahoo.co.jp>
2020-11-12 09:56:50 +0900

    ~プロトタイプ関連実装完了

--- db/migrate/20201110064739_create_prototypes.rb
+++ db/migrate/20201110064739_create_prototypes.rb
@@ -1,7 +1,7 @@
 class CreatePrototypes < ActiveRecord::Migration[6.0]
   def change
     create_table :prototypes do |t|
-      t.string :tittle,   null: false
+      t.string :title,   null: false
       t.text :catch_copy, null: false
       t.text :concept,    null: false
       t.references :user, foreign_key: true #userを参照するための外部キー
